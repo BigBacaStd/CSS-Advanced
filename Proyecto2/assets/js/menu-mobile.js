298b4f981d97590c066807fa7878e6d3c1ce55c5
PoloBaca <thebigbaca@gmail.com>
2024-04-25 20:30:35 -0600

    commit April 25 2024

---
+++ Proyecto2/assets/js/menu-mobile.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
+    let mobile_btn = document.querySelector(".navbar__mobile-btn");
+    let mobile_menu = document.querySelector(".menu-mobile");
+
+    mobile_btn.addEventListener("click", ()=>{
+        let show = document.querySelector(".menu-mobile--show");
+
+        if (show){
+            mobile_menu.classList.remove("menu-mobile--show");
+        }else{
+            mobile_menu.classList.add("menu-mobile--show");
+        }
+    });
+
+    window.addEventListener("resize", () =>{
+        let window_width = parseInt(document.body.clientWidth);
+
+        if(window_width >= 1000){
+            mobile_menu.classList.remove("menu-mobile--show");
+        }
+    });
+});
