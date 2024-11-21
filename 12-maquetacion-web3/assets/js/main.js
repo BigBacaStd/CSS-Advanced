cead28f9fdbb37065b6358643d3a6628afb2d014
PoloBaca <thebigbaca@gmail.com>
2024-11-20 21:19:42 -0600

    commit Nov 20 2024

--- 12-maquetacion-web3/assets/js/main.js
+++ 12-maquetacion-web3/assets/js/main.js
@@ -3,8 +3,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let btn_search = document.querySelector(".navbar__button");
 
     btn_search.addEventListener("click", () => {
-        input_search.style.width = "0";
-        input_search.style.padding = "0";
-        input_search.classList.remove("visible");
+        let visible = document.querySelector(".visible");
+
+        if (visible) {
+            input_search.style.width = "0";
+            input_search.style.padding = "0";
+            input_search.classList.remove("visible");
+        } else {
+            input_search.style.width = "15rem";
+            input_search.style.padding = "0.5rem 0 0.5rem 0.5rem";
+            input_search.classList.add("visible");
+        }
+
+
     });
 });
